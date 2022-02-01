f4c21eb53beae6a773b44ec19e7475e04f0fb0d7
ronit#123 <ronitwaje23>
2022-02-01 17:18:20 +0530

    interface aproach

---
+++ EmpAttendance/EmpAttendance/IEmpWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpAttendance
+{
+    internal interface IEmpWage
+    {
+        void AddCompany(string company, int wagePrHrs, int totalWorkHrs, int totalWorkDay);
+        void GetWage();
+    }
+}
+    
+
